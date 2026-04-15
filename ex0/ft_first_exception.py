120f4ec25d1628d7fef756732100b2f79250e02d
todina-r <ratsinjomananatolotra@gmail.com>
2026-04-16 00:26:16 +0300

    restructuration and remove usage of raise

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,21 +1,27 @@
 def input_temperature(temp_str: str) -> int:
     return int(temp_str)
 
+def test_temperature() -> None:
+    temp: int
+    data: str
 
-def test_data_as_input_temperature(data: str) -> None:
+    data = "25"
     print(f"Input data is '{data}'")
     try:
-        res: int = input_temperature(data)
-    except ValueError:
-        print("Caught input_temperature error:",
-              f"invalid literal for int() with base 10: '{data}'")
+        temp = input_temperature(data)
+    except ValueError as error:
+        print("Caught input_temperature error:", error)
     else:
-        print(f"Temperature is now {res}°C")
+        print(f"Temperature is now {temp}°C")
 
-
-def test_temperature() -> None:
-    test_data_as_input_temperature("25")
-    test_data_as_input_temperature("abc")
+    data = "abc"
+    print(f"Input data is '{data}'")
+    try:
+        temp = input_temperature(data)
+    except ValueError as error:
+        print("Caught input_temperature error:", error)
+    else:
+        print(f"Temperature is now {temp}°C")
 
 
 if __name__ == "__main__":
